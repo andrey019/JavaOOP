3d5cf772f5178854843fa9efbbfb0137ed860860
andrey019 <andrey019@gmail.com>
2016-04-29 16:59:37 +0300

    commit

--- src/HomeWork/Lesson8/Task5/Main.java
+++ src/HomeWork/Lesson8/Task5/Main.java
@@ -2,12 +2,34 @@ package HomeWork.Lesson8.Task5;
 
 class Main {
     public static void main(String[] args) {
-        MyArrayList list = new MyArrayList();
-        list.addElement("sdf");
-        list.addElement("rtrtr");
+        MyArrayList<String> list = new MyArrayList<>();
+        list.addElement("0000");
+        list.addElement("1111");
+        list.addElement("2222");
         list.addElement("3333");
+        list.addElement("4444");
+        list.addElement("1111");
+        list.addElement("5555");
 
-        System.out.println(list.getElement(1));
+        list.deleteElement(2);
 
+        System.out.println(list.contains("3333"));
+        System.out.println(list.indexOf("4444"));
+        System.out.println(list.lastIndexOf("1111"));
+
+        MyArrayList<Integer> listInt = new MyArrayList<>();
+        listInt.addElement(0);
+        listInt.addElement(1);
+        listInt.addElement(2);
+        listInt.addElement(3);
+        listInt.addElement(4);
+        listInt.addElement(5);
+        listInt.addElement(6);
+
+        Integer integer = listInt.getElement(4);
+
+        System.out.println(integer);
+        System.out.println(listInt.toString());
+        System.out.println(listInt.size());
     }
 }
--- src/HomeWork/Lesson8/Task5/MyArrayList.java
+++ src/HomeWork/Lesson8/Task5/MyArrayList.java
@@ -18,15 +18,82 @@ class MyArrayList<A> {
         }
     }
 
+    public void deleteElement(int index) {
+        Object[] temp = new Object[array.length - 1];
+        if (index == 0) {
+            System.arraycopy(array, 1, temp, 0, temp.length);
+            array = temp;
+        } else if (index == array.length - 1) {
+            System.arraycopy(array, 0, temp, 0, temp.length);
+            array = temp;
+        } else {
+            System.arraycopy(array, 0, temp, 0, index);
+            System.arraycopy(array, index + 1, temp, index, temp.length - index);
+            array = temp;
+        }
+    }
+
+    public boolean contains(A value) {
+        if (indexOf(value) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(A value) {
+        if (array[0].getClass() == "string".getClass()) {
+            String strValue = (String) value;
+            for (int i = 0; i < array.length; i++) {
+                String temp = (String) array[i];
+                if (temp.equalsIgnoreCase(strValue)) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == value) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(A value) {
+        if (array[0].getClass() == "string".getClass()) {
+            String strValue = (String) value;
+            for (int i = array.length - 1; i >= 0; i--) {
+                String temp = (String) array[i];
+                if (temp.equalsIgnoreCase(strValue)) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = array.length - 1; i >= 0; i--) {
+                if (array[i] == value) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
     public A getElement(int index) {
         return (A) array[index];
     }
-//
-//    public void add(Object value) {
-//        A[] temp;
-//        temp = new value[array.length + 1];
-//        temp[array.length + 1] = value;
-//        System.arraycopy(array, 0, temp, 0, temp.length);
-//        array = temp;
-//    }
+
+    public int size() {
+        return array.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[" + array[0] + ", ");
+        for (int i = 1; i < array.length - 1; i++) {
+            stringBuilder.append(array[i] + ", ");
+        }
+        stringBuilder.append(array[array.length - 1] + "]");
+        return stringBuilder.toString();
+    }
 }
