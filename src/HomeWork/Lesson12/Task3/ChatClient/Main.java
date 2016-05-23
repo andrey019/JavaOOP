0fe775a6b91bb46084df4c874eda9e7773bfa00d
andrey019 <andrey019@gmail.com>
2016-05-23 22:41:51 +0300

    commit

--- src/HomeWork/Lesson12/Task3/ChatClient/Main.java
+++ src/HomeWork/Lesson12/Task3/ChatClient/Main.java
@@ -1,6 +1,8 @@
 package HomeWork.Lesson12.Task3.ChatClient;
 
 import HomeWork.Lesson12.Task3.ChatServer.Message;
+
+import java.io.File;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -8,6 +10,39 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
+    private static Message createMessage(String s, String login) {
+        int del = s.indexOf(':');
+        String to = "";
+        String text = s;
+
+        if (del >= 0) {
+            to = s.substring(0, del);
+            text = s.substring(del + 1);
+        }
+
+        Message m = new Message();
+        m.text = text;
+        m.from = login;
+        m.to = to;
+        return m;
+    }
+
+    private static Message createFileMessage(String login, Scanner scanner) {
+        System.out.print("Type in full file path: ");
+
+        Message m = new Message();
+        if (m.attachFile(scanner.nextLine())) {
+            System.out.print("Type in to whom you want to send file: ");
+            m.to = scanner.nextLine();
+            m.from = login;
+            m.isFile = true;
+            return m;
+        } else {
+            System.out.println("No such file!");
+            return null;
+        }
+    }
+
 	public static void main(String[] args) {
 		try {
 			final Scanner scanner = new Scanner(System.in);
@@ -47,25 +82,15 @@ public class Main {
 					String s = scanner.nextLine();
 					if (s.isEmpty())
 						break;
-
+                    Message m = null;
                     if (s.equalsIgnoreCase("!sendfile")) {
-
+                        m = createFileMessage(login, scanner);
+                    } else {
+                        m = createMessage(s, login);
                     }
-					int del = s.indexOf(':');
-					String to = "";
-					String text = s;
-					
-					if (del >= 0) {
-						to = s.substring(0, del);
-						text = s.substring(del + 1);
-					}
-					
-					Message m = new Message();
-					m.text = text;
-					m.from = login;
-					m.to = to;
-					
-					m.writeToStream(os);
+                    if (m != null) {
+                        m.writeToStream(os);
+                    }
 				}
 			} finally {
 				th.interrupt();
